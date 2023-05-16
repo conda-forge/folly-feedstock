df55362ff7bae3cec143bb49eac6de9488dd8b55
Thorsten Beier <derthorstenbeier@gmail.com>
2023-05-16 12:09:24 +0200

    Update recipe/bld.bat
    Co-authored-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,8 @@
-cmake -G "NMake Makefiles" -D DBUILD_SHARED_LIBS=OFF -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR%
+cmake -G "NMake Makefiles" ^
+      -DBoost_NO_BOOST_CMAKE=ON ^
+      -D DBUILD_SHARED_LIBS=OFF ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
 nmake
